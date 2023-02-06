037d0990ae8eea062d9b6757b086a00c46baa6c1
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-02-06 20:50:45 +0200

    1111

--- js/01_work.js
+++ js/01_work.js
@@ -1,9 +1,16 @@
-// const x = 5;
-// const y = 8;
-// console.log(y);
-// console.log(x);
+const x = 5;
+const y = 8;
+console.log(y);
+console.log(x);
+x + y;
+console.log(x + y)
+// function calculateEngravingPrice(message, pricePerWord) {
 
+//     calculateEngravingPrice = message.length * pricePerWord;
+//     return calculateEngravingPrice;
 
+// }
+// console.log(calculateEngravingPrice)
 
 
 
