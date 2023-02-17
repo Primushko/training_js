e12a7e448292f18587ddc5c1105e0a2517dd17c0
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-02-17 17:20:27 +0200

    789456123

--- js/01_work.js
+++ js/01_work.js
@@ -1,29 +1,8 @@
 
-// console.log(`hello`)
-const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-    ];
-    
-    function getAllPropValues(propName) {
-    // Change code below this line
-    const category = [];
-    for (const product of products) {
-    if (product === propName) {
-        category.push(propName.product);
-        console.log(products);
-    }
-    // return category;
-    }
-    
-    
-    
-    
-    // Change code above this line
-    }
-    
+const numbers = [1, 2, 3].concat([4, 5, 6], [7, 8, 9]);
+
+console.log(numbers);
+
 
 
     
