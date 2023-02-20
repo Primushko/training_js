5d9d3d3f99f75e6a91bba150bd0b140163417d56
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-02-20 15:29:09 +0200

    тема дз4

--- js/01_work.js
+++ js/01_work.js
@@ -25,19 +25,90 @@
 // const c = { ...a, ...b, };
 // console.log(c);
 // ПРИКЛАД:
-const defaultSettings = {
-    theme: `light`,
-    showMotifications: true,
-    hideSidebar: false,
-};
+// const defaultSettings = {
+//     theme: `light`,
+//     showMotifications: true,
+//     hideSidebar: false,
+// };
 
-const userSettings = {
-    showMotifications: false,
-    hideSidebar: true,
-};
+// const userSettings = {
+//     showMotifications: false,
+//     hideSidebar: true,
+// };
 
-const finalSettings = {
-    ...defaultSettings,
-    ...userSettings,
-};
-console.log(finalSettings);
+// const finalSettings = {
+//     ...defaultSettings,
+//     ...userSettings,
+// };
+// console.log(finalSettings);
+
+
+// КОЛБЕК-ФУНКЦІЇ
+
+// function greet(name) {
+//     return `Ласкаво просимо ${name}.`;
+// }
+
+// Викликаємо функцію greet і виводимо результат у консоль
+// console.log(greet(`Mango`));
+
+// Виводимо функцію greet у консоль, не викликаючи її
+// console.log(greet);
+
+//                 Колбек-функція
+// function greet(name) {
+//     console.log(`Ласкаво просимо ${name}.`);
+// }
+//               Функція вищого порядку
+// function registerGuest(name, callback) {
+//     console.log(`Реєструємо гостя ${name}.`);
+//     callback(name);
+//     }
+// передали посилання на функцію greet як аргумент
+// registerGuest("Манго", greet);
+
+
+//                  ІНЛАЙН КОЛБЕКИ
+    // function registerGuest(name, callback) {
+    //     console.log(`Реєструємо гостя ${name}.`);
+    //     callback(name);
+    //     }
+        
+// Передаємо інлайн функцію greet у якості колбека
+    // registerGuest("Манго", function greet(name) {
+    // console.log(`Ласкаво просимо ${name}.`);
+    // });
+// Передаємо інлайн функцію notify у якості колбека
+    // registerGuest("Полі", function notify(name) {
+    // console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+    // });
+    //                 АБСТРАКЦІЯ
+    // function printValue(value) {
+    //     console.log(value);
+    // }
+    // function prettyPrint(value) {
+    //     console.log("Logging value: ", value);
+    // }
+    // function repeat(n, action) {
+    //     for (let i = 0; i < n; i += 1) {
+    //     action(i);
+    //     }
+    // }  
+// // Передаємо printValue як callback-функцію      
+//     repeat(3, printValue);
+// 0
+// 1
+// 2
+ // Передаємо prettyPrint як callback-функцію
+    // repeat(3, prettyPrint);
+// Logging value: 0
+// Logging value: 1
+// Logging value: 2   
+function makePizza() {
+    return "Your pizza is being prepared, please wait.";
+    }
+  // Change code below this line
+    
+    const result = makePizza();
+    const pointer = (makePizza);
+    console.log(makePizza(result));
